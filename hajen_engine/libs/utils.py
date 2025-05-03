7f01c67e4b133dc86f432b536812ee4acb9181f9
Jeremiah Payne <jeremiah@pinghajen.com>
2025-05-02 23:02:59 -0400

    Added create_task wrapper to utils.py

--- hajen_engine/libs/utils.py
+++ hajen_engine/libs/utils.py
@@ -1,6 +1,13 @@
-from multiprocessing import shared_memory
+import asyncio
+import functools
 import json
+import logging
 import struct
+
+from multiprocessing import shared_memory
+from typing import Coroutine, Optional
+
+logger = logging.getLogger(__name__)
 
 async def get_env_data():
     """
@@ -12,3 +19,38 @@ async def get_env_data():
         return json.loads(bytes(shm.buf[8:8 + size]).decode('utf-8'))
     finally:
         shm.close()
+
+def create_task(
+        coroutine: Coroutine,
+        name: str,
+        loop: Optional[asyncio.AbstractEventLoop] = None,
+    ) -> asyncio.Task:
+    """
+    This function adds a callback to the asyncio.create_task() to handle when a
+    task raises an exception
+    """
+    if loop is None:
+        loop = asyncio.get_running_loop()
+    task = loop.create_task(coroutine, name=name)
+    task.add_done_callback(
+        functools.partial(
+            _handle_task_result,
+        )
+    )
+    return task
+
+def _handle_task_result(
+        task: asyncio.Task,
+    ) -> None:
+    """
+    Checks `task.result()` for a result, if the result is
+    `asyncio.CancelledError` then pass and don't raise anything.
+    `asyncio.CancelledError` is raised when the task ends. All other errors
+    should be logged
+    """
+    try:
+        task.result()
+    except asyncio.CancelledError:
+        pass
+    except Exception:
+        logger.exception("Task raised an exception")
